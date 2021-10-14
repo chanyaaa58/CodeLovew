b703f1d39e48a22222c05dec65da828d12f7b28f
chanyaaa58 <kanegonaja@gmail.com>
2021-10-14 17:40:03 +0900

    [add]refs #8 setting devise messages to Japanese

--- Gemfile
+++ Gemfile
@@ -36,4 +36,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'dotenv-rails'
 gem 'devise'
+gem 'devise-i18n'
 gem 'rexml'
--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module OriginalApp
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
