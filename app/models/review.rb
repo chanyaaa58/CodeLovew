3279b1f6b0e12184b6c2c2a1dcb28f0c6f366fd5
chanyaaa58 <kanegonaja@gmail.com>
2021-10-15 19:55:40 +0900

    [fix]refs #11 set validation for Review model

---
+++ app/models/review.rb
@@ -0,0 +1,12 @@
+class Review < ApplicationRecord
+  with_options presence: true do
+    validates :name, length:{ in: 1..20 }
+    validates :title, length:{ in: 1..100 }
+    validates :problem, length:{ in: 1..3000 }
+    validates :detail, length:{ in: 1..3000 }
+    validates :content, length:{ in: 1..3000 }
+  end
+
+  validates :solution, length:{ maximum: 3000 }
+
+end
