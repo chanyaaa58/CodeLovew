1cf6ca774d0c0053dffef6430373e832a7a44a5d
chanyaaa58 <kanegonaja@gmail.com>
2021-10-17 01:48:07 +0900

    [fix]refs #21 redirect setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  before_action :user_check, only: [:show]
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_check
+    user = User.find(params[:id])
+    if current_user.id != user.id
+      redirect_to reviews_path, notice: "権限がありません"
+    end
   end
 end
