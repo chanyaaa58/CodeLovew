936e65fbdcd13cac6bf32d21baa23c183aa62609
chanyaaa58 <kanegonaja@gmail.com>
2021-10-17 01:39:58 +0900

    [fix]refs #21 create My page and fix the menu to be displayed before and after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後、reviews/indexに移動する
+  def after_sign_in_path_for(resource)
+    reviews_path
+  end
+
   protected
 
   def configure_permitted_parameters
