d1454525d9dbb2c3a0d478e8342992a2fc92d2cf
chanyaaa58 <kanegonaja@gmail.com>
2021-10-17 02:32:24 +0900

    [fix]refs #12 the respond_to method to comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  # コメントを保存、投稿するためのアクションです。
+  def create
+    # reviewをパラメータの値から探し出し,reviewに紐づくcommentsとしてbuildします。
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.build(comment_params)
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to review_path(@review) }
+      else
+        format.html { redirect_to review_path(@review), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+  private
+  # ストロングパラメーター
+  def comment_params
+    params.require(:comment).permit(:review_id, :content)
+  end
+end
