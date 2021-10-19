7eec57bc2fd56dbed9335d990bea23fac8769a58
chanyaaa58 <kanegonaja@gmail.com>
2021-10-19 13:41:45 +0900

    [fix]refs #17 set permissions for updating and deleting guest users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'tops#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    passwords: 'users/passwords',
     sessions: 'users/sessions'
   }
   resources :tops
