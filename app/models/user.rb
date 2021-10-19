7bd29e7ab245abe02973e31f585eae025a0089e2
chanyaaa58 <kanegonaja@gmail.com>
2021-10-19 13:29:28 +0900

    [fix]refs #17 setting up the guest login function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :icon, IconUploader
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "Guest"
+    end
+  end
 end
