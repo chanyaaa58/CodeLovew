cc1c8e3333f6b8a13f4c5bed399cf3bfb1119bf0
chanyaaa58 <kanegonaja@gmail.com>
2021-10-19 17:21:29 +0900

    [add]refs #34 add a user_id column to the reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_065735) do
+ActiveRecord::Schema.define(version: 2021_10_19_081347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2021_10_19_065735) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema.define(version: 2021_10_19_065735) do
   add_foreign_key "labellings", "reviews"
   add_foreign_key "lovews", "reviews"
   add_foreign_key "lovews", "users"
+  add_foreign_key "reviews", "users"
 end
